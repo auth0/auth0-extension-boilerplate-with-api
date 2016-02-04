310fdfb0a22adfd97645ce01b014485794b5e493
Javier Centurión <javier.centurion@auth0.com>
2016-02-04 11:40:55 -0300

    Adding JWT authentication

--- index.js
+++ index.js
@@ -1,22 +1,18 @@
-var express = require('express');
-var auth0   = require('auth0-oauth2-express');
-var Webtask = require('webtask-tools');
-var app     = express();
-var api     = express.Router();
+var express    = require('express');
+var auth0      = require('auth0-oauth2-express');
+var Webtask    = require('webtask-tools');
+var app        = express();
+var api        = express.Router();
+var jwtExpress = require('express-jwt');
+var unless     = require('express-unless');
+var jwt        = require('jsonwebtoken');
+var bodyParser = require('body-parser');
+
+app.use('/api', api);
 
 app.use(auth0({
   scopes: 'read:connections',
 }));
-
-app.use('/api', api);
-
-api.get('/secured', function (req, res) {
-  if (!req.user) {
-    return res.sendStatus(401);
-  }
-
-  res.status(200).send({message: 'Secured world'});
-});
 
 app.get('/', function (req, res) {
   var view = [
@@ -45,6 +41,56 @@ app.get('/', function (req, res) {
   res.header("Content-Type", 'text/html');
   res.status(200).send(view);
 });
+
+////////////// API //////////////
+if ((process.env.NODE_ENV || 'development') === 'development') {
+  var token = require('crypto').randomBytes(32).toString('hex');
+
+  api.use(function (req, res, next) {
+    req.webtaskContext = {
+      data: {
+        TOKEN_SECRET: token // This will be automatically provisioned once the extensions is installed
+      }
+    };
+
+    next();
+  });
+}
+
+var jwtChecker = jwtExpress({
+  secret: function(req, payload, done) {
+    done(null, req.webtaskContext.data.TOKEN_SECRET);
+  },
+  algorithms: ['HS256']
+});
+
+jwtChecker.unless = unless;
+
+api.use(bodyParser.json());
+api.use(jwtChecker.unless({path: '/api/login' }));
+
+api.post('/login', function (req, res) {
+  var secret = req.webtaskContext.data.TOKEN_SECRET;
+  var token  = jwt.sign(req.body, secret, {
+    algorithm: 'HS256',
+    issuer: 'http://localhost:3000',
+    audience: 'http://localhost:3000/api'
+  });
+
+  res.status(200)
+    .send({
+      token: token
+    });
+});
+
+api.get('/secured', function (req, res) {
+  if (!req.user) {
+    return res.sendStatus(401);
+  }
+
+  res.status(200).send({user: req.user});
+});
+////////////// API //////////////
 
 if ((process.env.NODE_ENV || 'development') === 'development') {
   app.listen(3000);
